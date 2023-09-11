c287ca0b74882f2f5a0ac4c8aa2114f03ad7f6dc
daiboom <dbchoi@aboutfishing.kr>
2023-09-11 21:50:26 +0900

    tst

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   experimental: {
-    // appDir: true,
+    appDir: true,
   },
 }
 
